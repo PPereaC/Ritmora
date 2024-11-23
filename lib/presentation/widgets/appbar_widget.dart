2b565416be83b440adefa8bb75157eb0976215a7
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2024-11-23 12:43:01 +0100

    update: Icono de búsqueda cambiado al appbar

--- lib/presentation/widgets/appbar_widget.dart
+++ lib/presentation/widgets/appbar_widget.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:icons_plus/icons_plus.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+import '../../domain/entities/song.dart';
+import '../delegates/search_songs_delegate.dart';
+import '../providers/providers.dart';
+
+class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AppBar(
       backgroundColor: Colors.grey[900],
       elevation: 0,
@@ -34,6 +40,30 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
               letterSpacing: 0.5,
             ),
           ),
+
+          const Spacer(),
+
+          // Botón de búsqueda
+          IconButton(
+            onPressed: () async {
+              final searchedSongs = ref.read(searchSongsProvider);
+              final searchQuery = ref.read(searchQueryProvider);
+              final song = await showSearch<Song?>(
+                query: searchQuery,
+                context: context,
+                delegate: SearchSongsDelegate(
+                  initialSongs: searchedSongs,
+                  searchSongs: ref.read(searchSongsProvider.notifier).searchSongsByQuery
+                )
+              );
+            },
+            icon: const Icon(
+              Iconsax.search_normal_1_outline,
+              color: Colors.white,
+              size: 25,
+            ),
+          ),
+
         ],
       ),
     );
--- lib/presentation/widgets/widget.dart
+++ lib/presentation/widgets/widget.dart
@@ -1,2 +1,3 @@
 export 'navbar_widget.dart';
 export 'appbar_widget.dart';
+export 'player_control_widget.dart';
