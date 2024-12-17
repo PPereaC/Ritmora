083c6ca60341d10f7f60ef1ac9c04b05a9709317
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2024-12-17 19:53:29 +0100

    update: Pantalla de inicio
    - Botón para cambiar el tema de la app.
    - Título de la ventana.

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeView extends StatelessWidget {
+import '../providers/providers.dart';
+
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Pantalla de Inicio'),
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final isDarkMode = ref.watch(isDarkmodeProvider);
+    final themeProvider = ref.watch(themeNotifierProvider.notifier);
+
+    return Scaffold(
+      backgroundColor: isDarkMode ? Colors.grey[900] : Colors.grey[100],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
+        title: Text(
+          'Inicio',
+          style: TextStyle(
+            color: isDarkMode ? Colors.white : Colors.black87,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => themeProvider.toggleDarkmode(),
+            icon: Icon(
+              isDarkMode ? Icons.light_mode_rounded : Icons.dark_mode_rounded,
+              color: isDarkMode ? Colors.white : Colors.grey,
+              size: 24,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/views/views.dart
+++ lib/presentation/views/views.dart
@@ -1,4 +1,3 @@
 export 'home_view.dart';
-export 'settings_view.dart';
 export 'library_view.dart';
 export 'search_view.dart';
