fed8add92db7d2b0e951e2ce3ac2c3b8e245ca2a
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-01-23 18:57:09 +0100

    fix: Cambiado a yt explode para reproducir canciones
    Solución temporal para reproducir las canciones sin restricciones.

--- lib/config/utils/background_tasks.dart
+++ lib/config/utils/background_tasks.dart
@@ -1,18 +1,29 @@
 // Función para obtener los stream urls de las canciones en segundo plano
-import 'dart:isolate';
-
 import 'package:finmusic/config/utils/pretty_print.dart';
+import '../../infrastructure/services/youtube_service.dart';
 
-import '../../infrastructure/models/player_response.dart';
+// Future<String?> getStreamUrlInBackground(String songId) async {
+//   printINFO(songId);
+//   return await Isolate.run(() async {
+//     final playerResponse = await PlayerResponse.fetch(songId, option: 0);
+//     if (playerResponse != null && playerResponse.playable) {
+//       final highestQualityAudio = playerResponse.highestQualityAudio;
+//       printINFO('URL: ${highestQualityAudio.url}');
+//       return highestQualityAudio.url;
+//     }
+//     return null;
+//   });
+// }
 
 Future<String?> getStreamUrlInBackground(String songId) async {
-  printINFO(songId);
-  return await Isolate.run(() async {
-    final playerResponse = await PlayerResponse.fetch(songId, option: 0);
-    if (playerResponse != null && playerResponse.playable) {
-      final highestQualityAudio = playerResponse.highestQualityAudio;
-      return highestQualityAudio.url;
-    }
+  try {
+    
+    final ytService = YoutubeService();
+    final url = await ytService.getCachedStreamUrl(songId);
+    
+    return url;
+  } catch (e) {
+    printERROR('Error getting stream URL: $e');
     return null;
-  });
+  }
 }
