9f2c1643eddcc1d9b3b3128021d2b44e9b84d6fb
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-03-31 14:49:13 +0200

    fix: Espacio vacio arriba del music control en homeview

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -24,129 +24,126 @@ class HomeViewState extends ConsumerState<HomeView> {
     // Cargar datos iniciales
     ref.read(trendingSongsProvider.notifier).loadSongs();
     ref.read(quickPicksProvider.notifier).loadSongs();
-    ref.read(homePlaylistsProvider.notifier).loadSongs();
+    ref.read(homePlaylistsProvider.notifier).  loadSongs();
   }
 
   @override
   Widget build(BuildContext context) {
-
     // Estados de las diferentes listas
     final trendingSongs = ref.watch(trendingSongsProvider);
     final quickPicks = ref.watch(quickPicksProvider);
     final homePlaylists = ref.watch(homePlaylistsProvider);
 
-    return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (index == 0) ...[
-                            if (Responsive.isMobile(context))
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                                        
-                                    const Padding(
-                                      padding: EdgeInsets.only(left: 8),
-                                      child: Text(
-                                        'FinMusic',
-                                        style: TextStyle(
-                                          fontFamily: 'Titulo',
-                                          color: Colors.white,
-                                          fontSize: 30
-                                        ),
-                                      )
-                                    ),
-                                                        
-                                    const Spacer(),
-                                                        
-                                    IconButton(
-                                      icon: const Icon(
-                                        Iconsax.notification_outline,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {},
-                                    ),
-
-                                    IconButton(
-                                      icon: const Icon(
-                                        Iconsax.repeate_music_outline,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {},
-                                    ),
-
-                                    IconButton(
-                                      icon: const Icon(
-                                        Iconsax.setting_2_outline,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {},
-                                    ),
-
-                                  ],
-                                ),
-                              ),
-                            
-                            if (Responsive.isDesktop(context))
-                              const SizedBox(height: 10),
-                            
-                          ],
-                            
-                          if (index == 1) ...[
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 5),
-                              child: Column(
-                                children: [
-                                  const _SectionTitle('Selecciones rápidas', showViewAll: false),
-                                  const SizedBox(height: 15),
-                                  SongGridHorizontalListview(songs: quickPicks),
-                                
-                                  const _SectionTitle('En tendencia'),
-                                  const SizedBox(height: 15),
-                                  SongHorizontalListview(songs: trendingSongs),
-                                
-                                  // Iteramos sobre cada categoría de playlists
-                                  ...homePlaylists.entries.map((entry) => [
-                                    _SectionTitle(entry.key, showViewAll: false),
-                                    const SizedBox(height: 15),
-                                    PlaylistHorizontalListview(
-                                      onTap: (playlist) {
-                                        context.go(
-                                          '/library/playlist/1/${playlist.playlistId}',
-                                          extra: playlist
-                                        );
-                                      },
-                                      playlists: entry.value
-                                    ),
-                                    const SizedBox(height: 20), // Espaciado entre secciones
-                                  ]).expand((widgets) => widgets),
-                                ],
-                              ),
-                            )
-                            
-                          ],
-                        ],
-                      );
-                    },
-                    childCount: 2,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: 10,
+              color: Theme.of(context).scaffoldBackgroundColor,
+            ),
+          ),
+          SliverAppBar(
+            floating: true,
+            pinned: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 8),
+                        child: Text(
+                          'FinMusic',
+                          style: TextStyle(
+                            fontFamily: 'Titulo',
+                            color: Colors.white,
+                            fontSize: 30
+                          ),
+                        )
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        icon: const Icon(
+                          Iconsax.notification_outline,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Iconsax.repeate_music_outline,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Iconsax.setting_2_outline,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const _SectionTitle('Selecciones rápidas', showViewAll: false),
+                      const SizedBox(height: 15),
+                      SongGridHorizontalListview(songs: quickPicks),
+                      
+                      const _SectionTitle('En tendencia'),
+                      const SizedBox(height: 15),
+                      SongHorizontalListview(songs: trendingSongs),
+                      
+                      // Iteramos sobre cada categoría de playlists
+                      ...homePlaylists.entries.map((entry) => [
+                        _SectionTitle(entry.key, showViewAll: false),
+                        const SizedBox(height: 15),
+                        PlaylistHorizontalListview(
+                          onTap: (playlist) {
+                            context.go(
+                              '/library/playlist/1/${playlist.playlistId}',
+                              extra: playlist
+                            );
+                          },
+                          playlists: entry.value
+                        ),
+                        const SizedBox(height: 20), // Espaciado entre secciones
+                      ]).expand((widgets) => widgets),
+                    ],
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 70,
+              color: Theme.of(context).scaffoldBackgroundColor,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
+// _SectionTitle class remains unchanged
 class _SectionTitle extends StatelessWidget {
   final String title;
   final bool showViewAll;
@@ -172,7 +169,7 @@ class _SectionTitle extends StatelessWidget {
                 ? SizedBox(
                     height: textStyle.titleLarge?.fontSize != null
                         ? textStyle.titleLarge!.fontSize! * 1.5
-                        : 24.0, // Define una altura adecuada
+                        : 24.0,
                     child: Marquee(
                       text: title,
                       style: textStyle.titleLarge?.copyWith(
@@ -199,29 +196,6 @@ class _SectionTitle extends StatelessWidget {
                     maxLines: 2,
                   ),
           ),
-
-          // if (showViewAll)
-          //   InkWell(
-          //     onTap: () {
-          //       // TODO: Implementar navegación
-          //     },
-          //     child: Row(
-          //       children: [
-          //         Text(
-          //           'Ver todo',
-          //           style: textStyle.bodyLarge?.copyWith(
-          //             color: Colors.white
-          //           ),
-          //         ),
-          //         const SizedBox(width: 5),
-          //         const Icon(
-          //           Icons.arrow_forward_ios_rounded,
-          //           size: 16,
-          //           color: Colors.white
-          //         ),
-          //       ],
-          //     ),
-          //   ),
         ],
       ),
     );
